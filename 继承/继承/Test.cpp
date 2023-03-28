6dc1f5d8765ca9a019934c6d744ec4fb5c1129fb
zhengyiq <601666966@qq.com>
2023-03-28 20:04:38 +0800

    菱形继承

--- 继承/继承/Test.cpp
+++ 继承/继承/Test.cpp
@@ -371,3 +371,106 @@
 
 
 
+#include <iostream>
+
+using namespace std;
+
+//class Person
+//{
+//public:
+//	string _name; // 姓名
+//};
+//class Student : virtual public Person
+//{
+//protected:
+//	int _num; //学号
+//};
+//class Teacher : virtual public Person
+//{
+//protected:
+//	int _id; // 职工编号
+//};
+//class Assistant : public Student, public Teacher
+//{
+//protected:
+//	string _majorCourse; // 主修课程
+//};
+//void Test()
+//{
+//	// 这样会有二义性无法明确知道访问的是哪一个
+//	Assistant a;
+//	//a._name = "peter"; // err
+//	// 需要显示指定访问哪个父类的成员可以解决二义性问题，但是数据冗余问题无法解决
+//	a.Student::_name = "xxx";
+//	a.Teacher::_name = "yyy";
+//}
+
+class A
+{
+public:
+	int _a1;
+	int* _a3;
+};
+
+// class B : public A
+class B : virtual public A
+{
+public:
+	int _b;
+};
+
+// class C : public A
+class C : virtual public A
+{
+public:
+	int _c;
+};
+
+class D : public B, public C
+{
+public:
+	int _d;
+};
+int main()
+{
+	int n = 2;
+	D d, d1, d2;
+	d.B::_a1 = 1;
+	d.C::_a3 = &n;
+	d._b = 3;
+	d._c = 4;
+	d._d = 5;
+
+
+	//B b;
+	//b._a = 10;
+	//b._b = 20;
+	return 0;
+}
+
+//// Car和BMW Car和Benz构成is-a的关系
+//class Car {
+//protected:
+//	string _colour = "白色"; // 颜色
+//	string _num = "陕ABIT00"; // 车牌号
+//};
+//class BMW : public Car {
+//public:
+//	void Drive() { cout << "好开-操控" << endl; }
+//};
+//class Benz : public Car {
+//public:
+//	void Drive() { cout << "好坐-舒适" << endl; }
+//};
+//// Tire和Car构成has-a的关系
+//class Tire {
+//protected:
+//	string _brand = "Michelin"; // 品牌
+//	size_t _size = 17; // 尺寸
+//};
+//class Car {
+//protected:
+//	string _colour = "白色"; // 颜色
+//	string _num = "陕ABIT00"; // 车牌号
+//	Tire _t; // 轮胎
+//};
