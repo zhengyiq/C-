aa325d0d5027b0cf0fbfc1de6d16670f11569db7
zyq <601666966@qq.com>
2023-06-12 21:50:51 +0800

    右值引用|移动构造

--- C++11/C++11/Test.cpp
+++ C++11/C++11/Test.cpp
@@ -1,8 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
 #include <vector>
 #include <list>
 #include <map>
+#include <string>
+#include <assert.h>
 using namespace std;
+#include "string.h"
+
+//struct Point
+//{
+//	int _x;
+//	int _y;
+//};
+//int main()
+//{
+//	int array1[] = { 1, 2, 3, 4, 5 };
+//	int array2[5] = { 0 };
+//	Point p = { 1, 2 };
+//	return 0;
+//}
 
 // 统一列表初始化
 // {}初始化
@@ -87,28 +104,126 @@ private:
 
 // decltype
 // decltype的一些使用使用场景
-template<class T1, class T2>
-void F(T1 t1, T2 t2)
-{
-	decltype(t1 * t2) ret;
-	cout << typeid(ret).name() << endl;
-}
-int main()
-{
-	const int x = 1;
-	double y = 2.2;
-	decltype(x * y) ret; // ret的类型是double
-	decltype(&x) p; // p的类型是int*
-	cout << typeid(ret).name() << endl;
-	cout << typeid(p).name() << endl;
-	F(1, 'a');
-
-	// vector存储的类型跟x*y表达式返回值类型一致
-	// decltype推导表达式类型，用这个类型实例化模板参数或者定义对象
-	vector<decltype(x* y)> v;
-	return 0;
-}
+//template<class T1, class T2>
+//void F(T1 t1, T2 t2)
+//{
+//	decltype(t1 * t2) ret;
+//	cout << typeid(ret).name() << endl;
+//}
+//int main()
+//{
+//	const int x = 1;
+//	double y = 2.2;
+//	decltype(x * y) ret; // ret的类型是double
+//	decltype(&x) p; // p的类型是int*
+//	cout << typeid(ret).name() << endl;
+//	cout << typeid(p).name() << endl;
+//	F(1, 'a');
+//
+//	// vector存储的类型跟x*y表达式返回值类型一致
+//	// decltype推导表达式类型，用这个类型实例化模板参数或者定义对象
+//	vector<decltype(x* y)> v;
+//	return 0;
+//}
 
 // 1. 增加支持initialzer_list的构造接口。使用更方便，有一定的价值
 // 2. 增加cbegin与cend系列迭代器的接口
 // 3. 移动构造和移动赋值
+
+//左值引用
+//int main()
+//{
+//	// 以下的p、b、c、*p都是左值
+//	int* p = new int(0);
+//	int b = 1;
+//	const int c = 2;
+//	// 以下几个是对上面左值的左值引用
+//	int*& rp = p;
+//	int& rb = b;
+//	const int& rc = c;
+//	int& pvalue = *p;
+//	return 0;
+//}
+//
+//右值引用
+//int main()
+//{
+//	double x = 1.1, y = 2.2;
+//	// 以下几个都是常见的右值
+//	10;
+//	x + y;
+//	fmin(x, y);
+//	// 以下几个都是对右值的右值引用
+//	int&& rr1 = 10;
+//	double&& rr2 = x + y;
+//	double&& rr3 = fmin(x, y);
+//	// 这里编译会报错：error C2106: “=”: 左操作数必须为左值
+//	10 = 1;
+//	x + y = 1;
+//	fmin(x, y) = 1;
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 1; 
+//	int* p = &a;
+//
+//	a + b;
+//
+//	// 左值引用给左值取别名
+//	int& ref1 = a;
+//	
+//	// 左值引用给右值取别名
+//	//int& ref2 = (a + b); err
+//	const int& ref = (a + b);
+//
+//	// 右值引用给右值取别名
+//	int&& ref3 = (a + b);
+//
+//	// 右值引用给左值取别名
+//	//int&& ref4 = a; err
+//	int&& ref4 = move(a);
+//
+//	return 0;
+//}
+
+//void func(const int& a)
+//{
+//	cout << "void func(const int& a)" << endl;
+//}
+//
+//void func(int& a)
+//{
+//	cout << "void func(int& a)" << endl;
+//}
+//
+//void func(int&& a)
+//{
+//	cout << "void func(int&& a)" << endl;
+//}
+//
+//int main()
+//{
+//	int a = 0; 
+//	int b = 1;
+//
+//	func(a);
+//	func(a + b); // 如果只有左值引用，且临时变量具有常性，那么由于权限的放大是无法进行传递的；如果要进行传递就需要在func的形参中添加const。但是这样的无法对传入的参数是左值还是右值进行区分。有了右值引用就可以将左值与右值进行区分。
+//}
+
+
+int  main()
+{
+	//右值：纯右值；将亡值
+	//左值引用已经将减少拷贝的场景进行处理，右值引用区分左值与右值，然后将右值(将亡值)的资源进行交换
+	//左值引用：直接减少拷贝。1.左值引用传参 2.传引用返回(函数类的局部对象不能使用引用返回)
+
+	//zyq::string s1("hello world");
+	//zyq::string ret1 = s1;
+	//zyq::string ret2 = (s1 + '!');
+	
+	zyq::string valStr = zyq::to_string(1234);
+	return 0;
+}
---
+++ C++11/C++11/string.h
@@ -0,0 +1,151 @@
+namespace zyq
+{
+	class string
+	{
+	public:
+		typedef char* iterator;
+		iterator begin()
+		{
+			return _str;
+		}
+
+		iterator end()
+		{
+			return _str + _size;
+		}
+
+		string(const char* str = "")
+			:_size(strlen(str))
+			, _capacity(_size)
+		{
+			//cout << "string(char* str)" << endl;
+
+			_str = new char[_capacity + 1];
+			strcpy(_str, str);
+		}
+
+		// s1.swap(s2)
+		void swap(string& s)
+		{
+			::swap(_str, s._str);
+			::swap(_size, s._size);
+			::swap(_capacity, s._capacity);
+		}
+
+		// 拷贝构造
+		string(const string& s)
+			:_str(nullptr)
+		{
+			cout << "string(const string& s) -- 深拷贝" << endl;
+
+			string tmp(s._str);
+			swap(tmp);
+		}
+
+		//// 移动构造
+		//string(string&& s)
+		//	:_str(nullptr)
+		//{
+		//	cout << "string(string&& s) -- 移动拷贝" << endl;
+		//	swap(s);
+		//}
+
+		// 赋值重载
+		string& operator=(const string& s)
+		{
+			cout << "string& operator=(string s) -- 深拷贝" << endl;
+			string tmp(s);
+			swap(tmp);
+
+			return *this;
+		}
+
+		~string()
+		{
+			delete[] _str;
+			_str = nullptr;
+		}
+
+		char& operator[](size_t pos)
+		{
+			assert(pos < _size);
+			return _str[pos];
+		}
+
+		void reserve(size_t n)
+		{
+			if (n > _capacity)
+			{
+				char* tmp = new char[n + 1];
+				strcpy(tmp, _str);
+				delete[] _str;
+				_str = tmp;
+
+				_capacity = n;
+			}
+		}
+
+		void push_back(char ch)
+		{
+			if (_size >= _capacity)
+			{
+				size_t newcapacity = _capacity == 0 ? 4 : _capacity * 2;
+				reserve(newcapacity);
+			}
+
+			_str[_size] = ch;
+			++_size;
+			_str[_size] = '\0';
+		}
+
+		//string operator+=(char ch)
+		string& operator+=(char ch)
+		{
+			push_back(ch);
+			return *this;
+		}
+
+		string operator+(char ch)
+		{
+			string tmp(*this);
+			tmp += ch;
+			return tmp;
+		}
+
+		const char* c_str() const
+		{
+			return _str;
+		}
+	private:
+		char* _str;
+		size_t _size;
+		size_t _capacity; // 不包含最后做标识的\0
+	};
+
+	zyq::string to_string(int value)
+	{
+		bool flag = true;
+		if (value < 0)
+		{
+			flag = false;
+			value = 0 - value;
+		}
+
+		zyq::string str;
+		while (value > 0)
+		{
+			int x = value % 10;
+			value /= 10;
+
+			str += ('0' + x);
+		}
+
+		if (flag == false)
+		{
+			str += '-';
+		}
+
+		std::reverse(str.begin(), str.end());
+		return str;
+	}
+}
