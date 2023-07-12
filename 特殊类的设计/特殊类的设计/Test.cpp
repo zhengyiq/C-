c150f5a4984972e9705ab3afbb869201f9779e46
zyq <601666966@qq.com>
2023-07-12 09:30:00 +0800

    单例模式 | 静态变量的线程安全问题

--- 特殊类的设计/特殊类的设计/Test.cpp
+++ 特殊类的设计/特殊类的设计/Test.cpp
@@ -169,94 +169,98 @@ private:
 // 饿汉的缺点：
 // 1、如果单例对象很大，main函数之前就要申请，第一，暂时不需要使用确占用资源，第二程序启动会受影响。
 // 2、如果两个单例都是饿汉，并且有依赖关系，要求单例1再创建，单例2再创建，饿汉无法控制顺序，懒汉才可以。
-// 饿汉的有点：
+// 饿汉的优点：
 // 简单（相对懒汉而言）
 
 // 懒汉完美的解决了上面饿汉的问题，但是就是相对复杂一点点
 
-class Singleton
-{
-public:
-	static Singleton* GetInstance()
-	{
-		// 双检查加锁
-		if (_ins == nullptr)
-		{
-			_imtx.lock(); // 只需要保护第一次
-
-			if (_ins == nullptr)
-			{ 
-				_ins = new Singleton;
-			}
-
-			_imtx.unlock();
-		}
-
-		return _ins;
-	}
-
-	// 一般全局都要使用单例对象，所以单例对象一般不需要显示释放
-	// 有些特殊场景，想显示释放一下，也可以手动释放。
-	static void DelInstance()
-	{
-		_imtx.lock();
-		if (_ins)
-		{
-			delete _ins;
-			_ins = nullptr;
-		}
-		_imtx.unlock();
-	}
-
-	// 单例对象回收
-	class GC
-	{
-	public:
-		~GC()
-		{
-			DelInstance();
-		}
-	};
-
-	static GC _gc;
-
-	void Add(const string& str)
-	{
-		_vmtx.lock();
-
-		_v.push_back(str);
-
-		_vmtx.unlock();
-	}
-
-	void Print()
-	{
-		_vmtx.lock();
-
-		for (auto& e : _v)
-		{
-			cout << e << endl;
-		}
-		cout << endl;
-
-		_vmtx.unlock();
-	}
-
-private:
-	// 限制类外面随意创建对象
-	Singleton()
-	{}
-private:
-	mutex _vmtx;
-	vector<string> _v;
-
-	static Singleton* _ins;
-	static mutex _imtx;
-};
-
-Singleton* Singleton::_ins = nullptr;
-mutex Singleton::_imtx;
-Singleton::GC Singleton::_gc;
+//class Singleton
+//{
+//public:
+//	static Singleton* GetInstance()
+//	{
+//		// 双检查加锁
+//		if (_ins == nullptr)
+//		{
+//			_imtx.lock(); // 只需要保护第一次
+//
+//			if (_ins == nullptr)
+//			{ 
+//				_ins = new Singleton;
+//			}
+//
+//			_imtx.unlock();
+//		}
+//
+//		return _ins;
+//	}
+//
+//	// 一般全局都要使用单例对象，所以单例对象一般不需要显示释放
+//	// 有些特殊场景，想显示释放一下，也可以手动释放。
+//	static void DelInstance()
+//	{
+//		_imtx.lock();
+//		if (_ins)
+//		{
+//			delete _ins;
+//			_ins = nullptr;
+//		}
+//		_imtx.unlock();
+//	}
+//
+//	// 单例对象回收
+//	class GC
+//	{
+//	public:
+//		~GC()
+//		{
+//			DelInstance();
+//		}
+//	};
+//
+//	static GC _gc;
+//
+//	void Add(const string& str)
+//	{
+//		_vmtx.lock();
+//
+//		_v.push_back(str);
+//
+//		_vmtx.unlock();
+//	}
+//
+//	void Print()
+//	{
+//		_vmtx.lock();
+//
+//		for (auto& e : _v)
+//		{
+//			cout << e << endl;
+//		}
+//		cout << endl;
+//
+//		_vmtx.unlock();
+//	}
+//
+//private:
+//	// 限制类外面随意创建对象
+//	Singleton() {}
+//
+//	// 防拷贝
+//	Singleton(const Singleton& s) = delete;
+//	Singleton& operator=(const Singleton& s) = delete;
+//
+//private:
+//	mutex _vmtx;
+//	vector<string> _v;
+//
+//	static Singleton* _ins;
+//	static mutex _imtx; // 这里有锁，而锁是不能够进行拷贝的，因此这个单例就是无法拷贝的
+//};
+//
+//Singleton* Singleton::_ins = nullptr; // 内置类型不会自己调用析构
+//mutex Singleton::_imtx;
+//Singleton::GC Singleton::_gc;
 
 //int main()
 //{
@@ -292,3 +296,59 @@ int main()
 
 	return 0;
 }
+
+// 保证一些数据(一个进程中)全局只有唯一一份， 并且方便访问
+// 1. 把这些数据放进一个类中，把这个类设计成单例类
+// 2. 构造和拷贝构造都封死，提供静态公有获取单例对象的函数
+// 3. 创建单例对象的方式：饿汉 懒汉
+
+
+// 这种方法是可以的，但是
+class Singleton
+{
+public:
+	static Singleton* GetInstance()
+	{
+		// C++11之前不能保证出初始化静态成员的线程安全问题
+		// C++11之后能保证出初始化静态成员的线程安全问题
+		static Singleton inst; // 这个静态变量在第一次调用的时候初始化
+		return &inst;
+	}
+
+	void Add(const string& str)
+	{
+		_vmtx.lock();
+
+		_v.push_back(str);
+
+		_vmtx.unlock();
+	}
+
+	void Print()
+	{
+		_vmtx.lock();
+
+		for (auto& e : _v)
+		{
+			cout << e << endl;
+		}
+		cout << endl;
+
+		_vmtx.unlock();
+	}
+
+private:
+	// 限制类外面随意创建对象
+	Singleton() 
+	{
+		cout << "Singleton()" << endl;
+	}
+
+	// 防拷贝
+	Singleton(const Singleton& s) = delete;
+	Singleton& operator=(const Singleton& s) = delete;
+
+private:
+	mutex _vmtx;
+	vector<string> _v;
+};
